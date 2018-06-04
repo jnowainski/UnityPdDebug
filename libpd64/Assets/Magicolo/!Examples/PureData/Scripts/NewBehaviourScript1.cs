0fe9d0eaed78e3c83f11a060164e53325177a3f3
jnowainski <jnowainski@techfak.uni-bielefeld.de>
2018-06-04 23:54:48 +0200

    initial commit adding files

---
+++ libpd64/Assets/Magicolo/!Examples/PureData/Scripts/NewBehaviourScript1.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Magicolo;
+
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    float btn_width, btn_height;
+    float field_width, field_height;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //Define button width and height
+        btn_width = 100;
+        btn_height = 50;
+        field_width = 100;
+        field_height = 25;
+        // Opening the patch will connect it up to the DSP
+        PureData.OpenPatch("abstract");
+    }
+
+    void OnGUI()
+    {
+
+        
+        //string field_duration = GUI.TextField(new Rect(100, 100, btn_width, btn_height), "Duration");
+        /*  
+        if (GUI.Button(new Rect(50,50,btn_width,btn_height),"Send Message"))
+        {
+            PureData.SendMessage("#touch", "abstract", 1.0, 0.5, 0.2, -30, 56, 12, 0.3, 3, 0.2, 0.5, 1.9);
+            PureData.SendMessage("#touch", "aTrigger", "bang");
+        } */
+    }
+}
---
+++ libpd64/Assets/Manager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI; //Need this for calling UI scripts
+using Magicolo;
+
+public class Manager : MonoBehaviour {
+    [SerializeField]
+    Transform UIPanel; //Will assign our panel to this variable so we can enable/disable it
+
+    [SerializeField]
+    InputField durationField, attackField, desvolField, pitchField, chirpField, lfndepthField, lfnfreqField,
+    amdepthField, amfreqField, richnessField;
+
+    float pointer, duration, attack, desvol, pitch, chirp, lfndepth, lfnfreq,
+    amdepth, amfreq, richness;
+
+    void Start () {
+        // Opening the patch will connect it up to the DSP
+        PureData.OpenPatch("abstract");
+
+        //initialize parameters
+        pointer = 1.0f;
+        duration = float.Parse(durationField.text);
+        attack = float.Parse(attackField.text);
+        desvol = float.Parse(desvolField.text);
+        pitch = float.Parse(pitchField.text);
+        chirp = float.Parse(chirpField.text);
+        lfndepth = float.Parse(lfndepthField.text);
+        lfnfreq = float.Parse(lfnfreqField.text);
+        amdepth = float.Parse(amdepthField.text);
+        amfreq = float.Parse(amfreqField.text);
+        richness = float.Parse(richnessField.text);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void changeValue()
+    {
+        duration = float.Parse(durationField.text);
+        attack = float.Parse(attackField.text);
+        desvol = float.Parse(desvolField.text);
+        pitch = float.Parse(pitchField.text);
+        chirp = float.Parse(chirpField.text);
+        lfndepth = float.Parse(lfndepthField.text);
+        lfnfreq = float.Parse(lfnfreqField.text);
+        amdepth = float.Parse(amdepthField.text);
+        amfreq = float.Parse(amfreqField.text);
+        richness = float.Parse(richnessField.text);
+
+        Debug.Log(richness);
+        Debug.Log(amfreq);
+        Debug.Log(amdepth);
+        Debug.Log(lfnfreq);
+        Debug.Log(lfndepth);
+
+        
+        PureData.SendMessage("#touch", "abstract", pointer, duration, attack, desvol, pitch, chirp, lfndepth, lfnfreq, amdepth, amfreq, richness);
+    }
+
+    public void sendMessage()
+    {
+        PureData.SendMessage("#touch", "aTrigger", "bang");
+    }
+}
